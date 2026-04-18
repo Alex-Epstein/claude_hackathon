4e6742fa08d78fa6f3cf57df37a0009337a1b002
Alex Epstein <zalexep1@gmail.com>
2026-04-18 11:03:32 -0400

    Initial Commit

---
+++ Appetight/AppetightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetightApp.swift
+//  Appetight
+//
+//  Created by Alexander Epstein on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct AppetightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
